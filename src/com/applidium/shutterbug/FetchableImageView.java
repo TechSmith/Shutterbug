7cd0901105a6185cd986db766a0ad6e47723d450
Adam Jensen <a.jensen@techsmith.com>
2013-02-25 15:11:47 -0500

    Add just-in-time image scaling to FetchableImageView

--- src/com/applidium/shutterbug/FetchableImageView.java
+++ src/com/applidium/shutterbug/FetchableImageView.java
@@ -5,6 +5,7 @@ import android.content.Context;
 import android.graphics.Bitmap;
 import android.graphics.drawable.ColorDrawable;
 import android.graphics.drawable.Drawable;
+import android.os.AsyncTask;
 import android.util.AttributeSet;
 import android.widget.ImageView;
 
@@ -12,6 +13,8 @@ import com.applidium.shutterbug.utils.ShutterbugManager;
 import com.applidium.shutterbug.utils.ShutterbugManager.ShutterbugManagerListener;
 
 public class FetchableImageView extends ImageView implements ShutterbugManagerListener {
+    private boolean mScaleImage = false;
+    
     public interface FetchableImageViewListener {
         void onImageFetched(Bitmap bitmap, String url);
 
@@ -33,17 +36,24 @@ public class FetchableImageView extends ImageView implements ShutterbugManagerLi
     }
 
     public void setImage(String url) {
-        setImage(url, new ColorDrawable(getContext().getResources().getColor(R.color.transparent)));
+        setImage(url, false, new ColorDrawable(getContext().getResources().getColor(R.color.transparent)));
+    }
+    
+    public void setImage(String url, boolean scaleImageToView) {
+        setImage(url, scaleImageToView, null);
     }
 
-    public void setImage(String url, int placeholderDrawableId) {
-        setImage(url, getContext().getResources().getDrawable(placeholderDrawableId));
+    public void setImage(String url, boolean scaleImageToView, int placeholderDrawableId) {
+        setImage(url, scaleImageToView, getContext().getResources().getDrawable(placeholderDrawableId));
     }
 
-    public void setImage(String url, Drawable placeholderDrawable) {
+    public void setImage(String url, boolean scaleImageToView, Drawable placeholderDrawable) {
+        mScaleImage = scaleImageToView;
         final ShutterbugManager manager = ShutterbugManager.getSharedImageManager(getContext());
         manager.cancel(this);
-        setImageDrawable(placeholderDrawable);
+        if (placeholderDrawable != null) {
+           setImageDrawable(placeholderDrawable);
+        }
         if (url != null) {
             manager.download(url, this);
         }
@@ -55,7 +65,12 @@ public class FetchableImageView extends ImageView implements ShutterbugManagerLi
 
     @Override
     public void onImageSuccess(ShutterbugManager imageManager, Bitmap bitmap, String url) {
-        setImageBitmap(bitmap);
+        if (mScaleImage) {
+            setImageBitmap(null);
+            new DownloadImageTask( getWidth(), getHeight(), bitmap ).execute();
+        } else {
+            setImageBitmap(bitmap);
+        }
         requestLayout();
         if (mListener != null) {
             mListener.onImageFetched(bitmap, url);
@@ -69,4 +84,49 @@ public class FetchableImageView extends ImageView implements ShutterbugManagerLi
         }
     }
 
+    public class DownloadImageTask extends AsyncTask<Void, Void, Bitmap> {
+       protected int                      mMaxWidth;
+       protected int                      mMaxHeight;
+       protected Bitmap mBitmap;
+       
+       public DownloadImageTask( int maxWidth, int maxHeight, Bitmap bitmap ) {
+          mMaxWidth = maxWidth;
+          mMaxHeight = maxHeight;
+          mBitmap = bitmap;
+       }
+
+       protected Bitmap doInBackground( Void... args ) {
+          Bitmap thumbnail;
+          if ( isCancelled() || mBitmap == null ) { return null; }
+
+          if ( mMaxWidth <= 0 && mMaxHeight <= 0 ) {
+             thumbnail = mBitmap;
+          } else {
+             float scaleX = Float.MAX_VALUE;
+             float scaleY = Float.MAX_VALUE;
+
+             if ( mMaxWidth > 0 ) {
+                scaleX = (float) mMaxWidth / (float) mBitmap.getWidth();
+             }
+
+             if ( mMaxHeight > 0 ) {
+                scaleY = (float) mMaxHeight / (float) mBitmap.getHeight();
+             }
+
+             float scale = Math.min( scaleX, scaleY );
+
+             thumbnail = Bitmap.createScaledBitmap(
+                   mBitmap,
+                   Math.round( mBitmap.getWidth() * scale ),
+                   Math.round( mBitmap.getHeight() * scale ),
+                   false );
+          }
+          
+          return thumbnail;
+       }
+       
+       protected void onPostExecute(Bitmap scaledBitmap) {
+           setImageBitmap(scaledBitmap);
+       }
+    }
 }
