ae9055f2cded58045ab7c505a5d6593719d5e576
Adam Jensen <a.jensen@techsmith.com>
2013-02-28 09:44:08 -0500

    Make LruCache::trimToSize() public

--- src/com/applidium/shutterbug/cache/LruCache.java
+++ src/com/applidium/shutterbug/cache/LruCache.java
@@ -137,7 +137,7 @@ public class LruCache<K, V> {
      *            the maximum size of the cache before returning. May be -1 to
      *            evict even 0-sized elements.
      */
-    private void trimToSize(int maxSize) {
+    public void trimToSize(int maxSize) {
         while (true) {
             K key;
             V value;
