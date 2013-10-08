0ead64e16a21127240847847d2a9bef13dffbda8
Chris Bowron <c.bowron@techsmith.com>
2013-10-08 10:16:47 -0400

    build target 17

--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
 #!/bin/bash -xe
 
 echo "Building Shutterbug"
-android update lib-project --target android-16 --path .
+android update lib-project --target android-17 --path .
 ant clean
 ant release
 
